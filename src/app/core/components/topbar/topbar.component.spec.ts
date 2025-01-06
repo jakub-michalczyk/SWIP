e5803ad28f7319dd63025565621d9719e3255587
Jakub <devcobbier@gmail.com>
2025-01-06 19:50:39 +0100

    implemnted homepage, mobile service, added internalization, lang buttons in topbar

---
+++ src/app/core/components/topbar/topbar.component.spec.ts
@@ -0,0 +1,74 @@
+import { TestBed } from '@angular/core/testing';
+import { TopbarComponent } from './topbar.component';
+import { TranslateService } from '@ngx-translate/core';
+import { Subject } from 'rxjs';
+import { ELanguageCode } from '../../../shared/enums/language.enum';
+import { MatButtonModule } from '@angular/material/button';
+
+describe('TopbarComponent', () => {
+  let component: TopbarComponent;
+  let translateServiceMock: jasmine.SpyObj<TranslateService>;
+  let onDefaultLangChange$: Subject<{ lang: string }>;
+
+  beforeEach(() => {
+    onDefaultLangChange$ = new Subject();
+
+    translateServiceMock = jasmine.createSpyObj('TranslateService', [
+      'setDefaultLang',
+    ]);
+
+    translateServiceMock.currentLang = ELanguageCode.EN;
+    translateServiceMock.defaultLang = ELanguageCode.EN;
+
+    Object.defineProperty(translateServiceMock, 'onDefaultLangChange', {
+      get: () => onDefaultLangChange$.asObservable(),
+    });
+
+    TestBed.configureTestingModule({
+      imports: [MatButtonModule],
+      providers: [
+        { provide: TranslateService, useValue: translateServiceMock },
+      ],
+    }).compileComponents();
+
+    const fixture = TestBed.createComponent(TopbarComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should initialize lang to currentLang or defaultLang on setup', () => {
+    translateServiceMock.currentLang = ELanguageCode.PL;
+    component.setUpLang();
+    expect(component.lang).toBe(ELanguageCode.PL);
+
+    translateServiceMock.currentLang = null as any;
+    translateServiceMock.defaultLang = ELanguageCode.EN;
+    component.setUpLang();
+    expect(component.lang).toBe(ELanguageCode.EN);
+  });
+
+  it('should update lang when onDefaultLangChange emits a new value', () => {
+    component.setUpLang();
+
+    onDefaultLangChange$.next({ lang: ELanguageCode.PL });
+    expect(component.lang).toBe(ELanguageCode.PL);
+
+    onDefaultLangChange$.next({ lang: ELanguageCode.EN });
+    expect(component.lang).toBe(ELanguageCode.EN);
+  });
+
+  it('should call TranslateService setDefaultLang when switchLanguage is invoked', () => {
+    component.switchLanguage(ELanguageCode.PL);
+    expect(translateServiceMock.setDefaultLang).toHaveBeenCalledWith(
+      ELanguageCode.PL
+    );
+
+    component.switchLanguage(ELanguageCode.EN);
+    expect(translateServiceMock.setDefaultLang).toHaveBeenCalledWith(
+      ELanguageCode.EN
+    );
+  });
+});
--- src/app/core/components/topbar/topbar.component.ts
+++ src/app/core/components/topbar/topbar.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatButtonModule } from '@angular/material/button';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { ELanguageCode } from '../../../shared/enums/language.enum';
 
 @Component({
   selector: 'swip-topbar',
-  imports: [],
+  imports: [MatButtonModule, TranslateModule],
   templateUrl: './topbar.component.html',
 })
-export class TopbarComponent {}
+export class TopbarComponent {
+  private readonly destroyRef = inject(DestroyRef);
+  lang: ELanguageCode = ELanguageCode.EN;
+
+  constructor(private translate: TranslateService) {
+    this.setUpLang();
+  }
+
+  setUpLang() {
+    this.lang =
+      (this.translate.currentLang as ELanguageCode) ||
+      (this.translate.defaultLang as ELanguageCode);
+    this.translate.onDefaultLangChange
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((langData) => {
+        this.lang = langData.lang as ELanguageCode;
+      });
+  }
+
+  switchLanguage(language: string): void {
+    this.translate.setDefaultLang(language);
+  }
+}

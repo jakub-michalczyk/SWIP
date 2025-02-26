7473878cd8d0207f121a66b8e368f78ca967dddc
Jakub <devcobbier@gmail.com>
2025-02-26 17:55:00 +0100

    adjusting discover more subpage

--- src/app/features/more/more/more.data.ts
+++ src/app/features/more/more/more.data.ts
@@ -3,18 +3,12 @@ import { IMoreSection } from './more.interface';
 export const MORE_SECTIONS: IMoreSection[] = [
   {
     index: 1,
-    titleCode: 'SCROLL_TITLE',
-    descriptionCode: 'SCROLL_DESC',
-    image: 'scroll',
-  },
-  {
-    index: 2,
     titleCode: 'SWIPE_LEFT_TITLE',
     descriptionCode: 'SWIPE_LEFT_DESC',
     image: 'swipe_left',
   },
   {
-    index: 3,
+    index: 2,
     titleCode: 'SWIPE_RIGHT_TITLE',
     descriptionCode: 'SWIPE_RIGHT_DESC',
     image: 'swipe_right',
